6a6d468d956f71a5d89e8b609f712d5ff7a2ec10
kimjuh620 <kimjuh620@gmail.com>
2025-10-14 20:55:05 +0900

    modify file name

---
+++ DFS/test.py
@@ -0,0 +1,5 @@
+n=int(input())
+
+score = list(map(int, input().split()))
+
+print(score)
--- greedy/3-4.py
+++ greedy/3-4.py
@@ -25,11 +25,11 @@ n, k = map(int, input().split())
 result = 0
 
 while True:
-    if n==1:
+    if n == 1:
         break
-    elif n%k == 0:
+    elif n % k == 0:
         result += 1
-        n = n/k
+        n = n / k
     else:
         result += 1
         n -= 1
